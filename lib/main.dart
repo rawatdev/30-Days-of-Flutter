054add8cb414499c716b2871dd0ad3a1481bc7bc
Abhishek <abhishekrawat708090@gmail.com>
2022-04-19 17:28:53 +0530

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
+import 'dart:developer';
+
 import "package:flutter/material.dart";
-import 'home_page.dart';
+import 'package:first_flutter_app/pages/home_page.dart';
+import 'package:first_flutter_app/pages/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: HomePage(),
-        );
+    return  MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage()
+      },
+    );
+  }
+
+  String bringVegetables({required bool thaila, int price = 100}) {
+    return "You Price $price and thaila $thaila";
   }
 }
